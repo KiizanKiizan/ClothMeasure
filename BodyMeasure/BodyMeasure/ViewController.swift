acef85c9404f88672825dd870e854a8684e9ab2d
hiroaki Iwai <hiro_lily_da@hotmail.com>
2018-06-04 22:37:50 +0900

    体のサイズ計測のデモアプリ用のプロジェクト追加

---
+++ BodyMeasure/BodyMeasure/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  BodyMeasure
+//
+//  Created by 岩井 宏晃 on 2018/06/04.
+//  Copyright © 2018年 kiizankiizan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
